74c3e03b66698dc47f0e4fbb1d13fcea28d384ac
Alexander Da Costa <bsmuse@tutamail.com>
2023-10-03 06:54:10 -0600

    Add function to generate alphanumeric string and post method in experss_server

--- express_server.js
+++ express_server.js
@@ -4,11 +4,24 @@ const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
+} 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,6 +31,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 }); 
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id; 
@@ -30,8 +47,12 @@ app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: url };
     res.render("urls_show", templateVars);
   }
-});
+}); 
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
